56a9c9521e3c2859757e5a38e79409aa2781173c
Tim Martin <insanitysfall@bigpond.com>
2017-12-16 13:11:32 +1000

    added views removed Project.html

--- Project.js
+++ Project.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
 	  $('#calendar').fullCalendar({
+   //      header: {
+			
+			// left: 'prev,next today',
+			// center: 'title',
+			// right: 'listDay,listWeek,listMonth'
+			// },
+			header: false,
+			defaultView: 'listWeek',
+			selectable: true,
+			selectHelper: true,
+			editable: true,
+			eventLimit: true
+    })
+
+
+
+	   $('#calendar-list').fullCalendar({
         header: {
 			left: 'prev,next today',
 			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
+			right: 'month,agendaWeek,agendaDay',
 			},
 			selectable: true,
 			selectHelper: true,
